8e03e1f6596544722ffa3189bbf7d69e5dbdaf82
hanxiuting <hanxiuting127@163.com>
2021-05-16 20:13:47 +0800

    项目代码

---
+++ week2/Project/Week02_NIO/src/main/java/com/geekbang/Week02_NIO/Week02NioApplication.java
@@ -0,0 +1,13 @@
+package com.geekbang.Week02_NIO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week02NioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week02NioApplication.class, args);
+	}
+
+}

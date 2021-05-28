d7118b1af1c54c9f76c867180d26639fee51b276
hanxiuting <hanxiuting127@163.com>
2021-05-28 19:37:42 +0800

    作业第2题

---
+++ week4/week04/src/main/java/com/geekbang/week04/entity/ResultEntity.java
@@ -0,0 +1,13 @@
+package com.geekbang.week04.entity;
+
+public class ResultEntity {
+    private int result;
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+}
